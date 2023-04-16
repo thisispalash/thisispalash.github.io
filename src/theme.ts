2931c7f4b31989d07e4b67cc060fc133e7980159
Palash A <palash.agarwal.96@gmail.com>
2023-04-16 11:50:03 -0400

    add theme

---
+++ src/theme.ts
@@ -0,0 +1,22 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const styles = {
+  global: {
+
+  }
+}
+
+const colors = {
+  bg: '#000000',
+  bgSecondary: '#0f0f0f',
+  text: '#eeeeee',
+  highlight: '#82ceaf',
+  bgGradient: 'linear-gradient(90deg, #65789a 0%, #0f0f0f 100%)',
+  bgAlt: '#5B6C8A'
+}
+
+const fonts = {
+  heading: 'Comfortaa, sans-serif',
+  default: 'Lato', 
+}
+
