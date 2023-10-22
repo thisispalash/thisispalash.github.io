ace6e67c72b76538a61bc99b9a4e00d77ac4dbb4
Palash A <thisispalash@kdio.xyz>
2023-10-22 11:02:34 +0530

    Presentable Portfolio (#6)
    * update socials and bio
    
    * add tooltip and toast
    
    * link out kdio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
-import { AspectRatio, Divider, HStack, Image, Link, Spacer, Text, useDisclosure, VStack } from '@chakra-ui/react';
+import { AspectRatio, Divider, HStack, Image, Link, Spacer, Text, Tooltip, useDisclosure, VStack } from '@chakra-ui/react';
 import Head from '@/components/Head';
 import ContactFormModal from '@/components/ContactFormModal';
+import { useGlobalContext } from '@/context/GlobalContext';
 
 export default function Home() {
 
   const { isOpen, onOpen, onClose } = useDisclosure();
+  
+  // @ts-ignore
+  const { makeToast } = useGlobalContext();
 
   const contact = () => {
     onOpen();
+  }
+
+  const canaryToast = () => {
+    makeToast({ code: 501, title: 'Canary Feature!' });
   }
 
   return (
@@ -21,21 +29,36 @@ export default function Home() {
         <HStack spacing={8} px={12} py={12} w='full'>
           <Spacer />
           <AspectRatio ratio={1} w='225px' h='225px'>
-            <Image src='/images/balli-no-bg.png' alt='thisispalash' onClick={contact} className='cursor-message' />
+            <Tooltip label='Contact Me' aria-label='Contact Me'>
+              <Image src='/images/balli-no-bg.png' alt='thisispalash' onClick={contact} className='cursor-message' />
+            </Tooltip>
           </AspectRatio>
           <VStack spacing={2} textAlign='left'>
             <Spacer />
             <Text w='full' fontSize='4xl' fontFamily='heading' fontWeight='semibold' color='highlight'>
-              thisispalash [
-              <Link href='https://twitter.com/kdiodna' isExternal variant='reverse' className='cursor-twitter'>d</Link>
-              <Link href='https://linkedin.com/in/isthispalash' isExternal variant='reverse' className='cursor-linkedin'>o</Link>
-              <Link href='https://twitter.com/theprimefibber' isExternal variant='reverse' className='cursor-twitter'>t</Link>
-              ] com
+              thisispalash{' '}
+              <Tooltip label='Spotify' aria-label='Spotify'>
+                <Link href='https://open.spotify.com/user/palash96_rox?si=ebe624cfcf7b47b9' isExternal variant='reverse' className='cursor-spotify'>[</Link>
+              </Tooltip>
+              <Tooltip label='Twitter / X' aria-label='Twitter / X'>
+                <Link href='https://twitter.com/kdiodna' isExternal variant='reverse' className='cursor-twitter'>d</Link>
+              </Tooltip>
+              <Tooltip label='LinkedIn' aria-label='LinkedIn'>
+                <Link href='https://linkedin.com/in/isthispalash' isExternal variant='reverse' className='cursor-linkedin'>o</Link>
+              </Tooltip>
+              <Tooltip label='X / Twitter' aria-label='X / Twitter'>
+                <Link href='https://twitter.com/theprimefibber' isExternal variant='reverse' className='cursor-twitter'>t</Link>
+              </Tooltip>
+              <Tooltip label='Instagram' aria-label='Instagram'>
+                <Link href='https://instagram.com/isthispalash' isExternal variant='reverse' className='cursor-instagram'>]</Link>
+              </Tooltip>
+              {' '}com
             </Text>
             <Text w='full' fontSize='lg'>
-              Decentralizing the world,{' '}
-              <Link href='https://github.com/thisispalash' isExternal className='cursor-github'>one Byte</Link>
-              {' '}at a time!
+              <Tooltip label='GitHub' aria-label='GitHub'>
+                <Link href='https://github.com/thisispalash' isExternal className='cursor-github'>Decentralizing</Link>
+              </Tooltip>
+              {' '}the world, one bit at a time!
             </Text>
             <Spacer />
           </VStack>
@@ -49,12 +72,12 @@ export default function Home() {
           <Divider px={32} />
           <HStack spacing={8} px={12} textAlign='center'>
             <Spacer />
-            <Link href='/kdio' variant='reverse'>
+            <Link href='https://kdio.xyz' variant='reverse'>
               <Text fontFamily='heading' fontSize='lg'>khaaliDimaag [dot] io</Text>
             </Link>
-            <Link href='/b3' variant='reverse'>
-              <Text fontFamily='heading' fontSize='lg'>Bedside Blackboard</Text>
-            </Link>
+            <Text fontFamily='heading' fontSize='lg' variant='cilckableReverse' onClick={canaryToast}>
+              Bedside Blackboard
+            </Text>
             <Spacer />
           </HStack>
         </VStack>
